0cf56a55bcf463f8bdfe28658e82dae4f657e0a0
Jefferson <jefferson.santossilva22@gmail.com>
2023-06-30 13:47:12 -0300

    segundo commit

--- caixa_eletro.py
+++ caixa_eletro.py
@@ -1,38 +1,56 @@
 saldo = 1000
+extrato = []
 
-
-def sacar(valor):
-    global saldo
+def Sacar(valor):
+    global saldo, extrato
+    
     if saldo >= valor:
         saldo -= valor
+        extrato.append(f"SAQUE: R$ {valor}")
         print(f'SAQUE REALIZADO NO VALOR DE R$ {valor}')
-        print(f'VALOR ATUAL DISPONIVEL NA CONTA R$ {saldo}')
+        print(f'VALOR ATUAL DISPONIVEL NA CONTA R$ {saldo}')    
     else:
         print('SALDO INSUFICIENTE')
         print(f"SALDO ATUAL DE R$  {saldo}")
 
-def depositar(valor):
-    global saldo
+def Depositar(valor):
+    global saldo, extrato
     saldo += valor
+    extrato.append(f"DEPOSITO: R$ {valor}")
     print(f'VALOR ATUAL DISPONIVEL NA CONTA R$ {saldo}')
 
+def Extrato():
+    for i in extrato:
+        print(i)
+
+limete_saq = 0
 while True:
     print("""========BANCO SILVA========
     
-    OPÇÃO[1] - SAQUE
+    OPÇÃO[1] - DEPOSITAR
 
-    OPÇÃO[2] - DEPOSITAR
+    OPÇÃO[2] - SACAR
 
-    OPÇAO[0] - EXIT
+    OPÇAO[3] - EXTRATO
+
+    OPCAO[0] - SAIR
     """)
     print('===========================')
-    opcao = int(input("DIGITE A SUA ESCOLHA: "))
-    if opcao == 1:
-        valor = float(input("VALOR DESEJADO R$ "))
-        sacar(valor)
-    elif opcao == 2:
-        valor = float(input("VALOR DO DEPOSITO R$ "))
-        depositar(valor)
-    else:
-        print("OBRIGADO, ATE MAIS!")
+    opcao = input("DIGITE A SUA ESCOLHA: ")
+    if opcao == "1" :
+        valor = float(input("VALOR DO DEPOSITO: R$ "))
+        Depositar(valor)
+    elif opcao == "2":
+        if limete_saq < 3:
+            valor = float(input("VALOR A SACAR: R$ "))
+            limete_saq += 1
+            Sacar(valor)
+    elif limete_saq > 3:
+        print("LIMITE DE SAQUE ATIGINDO!")
+    elif opcao == "3":
+        Extrato()
+    elif opcao =="0":
         break
+    else:
+        print("OPÇÃO INVALIDA, TENTE NOVAMENTE")
+        
