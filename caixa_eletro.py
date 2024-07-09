e6d1261090a5c60a7575098b62842f5762251a6a
Jefferson Santos Silva <84148608+JeffersonSnjolly@users.noreply.github.com>
2024-07-09 16:10:50 -0300

    Update caixa_eletro.py
    adicionando novas funcionalidades

--- caixa_eletro.py
+++ caixa_eletro.py
@@ -1,56 +1,119 @@
-saldo = 1000
-extrato = []
+import os
 
-def Sacar(valor):
-    global saldo, extrato
-    
-    if saldo >= valor:
-        saldo -= valor
-        extrato.append(f"SAQUE: R$ {valor}")
-        print(f'SAQUE REALIZADO NO VALOR DE R$ {valor}')
-        print(f'VALOR ATUAL DISPONIVEL NA CONTA R$ {saldo}')    
-    else:
-        print('SALDO INSUFICIENTE')
-        print(f"SALDO ATUAL DE R$  {saldo}")
+def menu():
+    MENU ="""======================Banco Silva Tech======================
 
-def Depositar(valor):
-    global saldo, extrato
-    saldo += valor
-    extrato.append(f"DEPOSITO: R$ {valor}")
-    print(f'VALOR ATUAL DISPONIVEL NA CONTA R$ {saldo}')
+        [D]  DEPOSITAR
+        [S]  SACAR
+        [E]  EXTRATO
+        [NC] NOVA CONTA
+        [LC] LISTAR CONTAS
+        [NU] NOVO USUÁRIO
+        [Q]  SAIR:
 
-def Extrato():
-    for i in extrato:
-        print(i)
+        ============================================================ 
+        DIGITE : """
+    return input(MENU)
 
-limete_saq = 0
+saldo = 0
+LIMITE_VALOR = 500
+extrato  = []
+saques = 0
+LIMITE_SAQUE = 3
+
 while True:
-    print("""========BANCO SILVA========
-    
-    OPÇÃO[1] - DEPOSITAR
-
-    OPÇÃO[2] - SACAR
-
-    OPÇAO[3] - EXTRATO
-
-    OPCAO[0] - SAIR
-    """)
-    print('===========================')
-    opcao = input("DIGITE A SUA ESCOLHA: ")
-    if opcao == "1" :
-        valor = float(input("VALOR DO DEPOSITO: R$ "))
-        Depositar(valor)
-    elif opcao == "2":
-        if limete_saq < 3:
-            valor = float(input("VALOR A SACAR: R$ "))
-            limete_saq += 1
-            Sacar(valor)
-    elif limete_saq > 3:
-        print("LIMITE DE SAQUE ATIGINDO!")
-    elif opcao == "3":
-        Extrato()
-    elif opcao =="0":
+    opcao = menu().upper()
+    os.system('cls')
+    if opcao == 'D':
+        while True:
+            try:    
+                valor  = float(input('DIGITE O VALOR DO DEPOSITO R$ '))
+                os.system('cls')
+                if valor > 0:
+                    config = input(f"""DEPOSITO NO VALOR DE  R${valor} ESTÁ CORRETO?
+                                    [S] SIM
+                                    [N] NÃO 
+                                    DIGITE : """).upper()
+                    os.system('cls')
+                    if config == 'S':
+                        saldo += valor
+                        extrato.append(f'DEPOSITO DE {valor:.2f}')
+                        print('SUCESSO NA OPERAÇÃO.')
+                        print(f'SALDO EM CONTA {saldo:.2f}')
+                        input('APERTE QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL... ')
+                        os.system('cls')
+                        break
+                    elif config == 'N':
+                        tentativa = input("""DESEJA TENTAR NOVAMENTE ?
+                                        [S] SIM
+                                        [N] NÃO
+                                        DIGITE : """).upper()
+                        os.system('cls')
+                        if tentativa != 'S':
+                            break     
+            except:                   
+                print('DIGITE APENAS VALORES NUMÉRICOS....')
+                input()
+                os.system('cls')
+                break                          
+    elif opcao == 'S':
+        while True:
+            try:
+                saque = float(input("VALOR DESEJADO R$ "))
+                os.system('cls')
+                config_2 = input(f"""SAQUE NO VALOR DE  R${saque} ESTÁ CORRETO?
+                                    [S] SIM
+                                    [N] NÃO 
+                                    DIGITE : """).upper()
+                os.system('cls')
+                if config_2 == 'S':
+                    if (saldo > 0) & (saldo >= saque) & (saque <= LIMITE_VALOR) & (saques < LIMITE_SAQUE):
+                        saldo -= saque
+                        extrato.append(f'SAQUE DE {saque:.2f}')
+                        saques +=1                    
+                        print('SAQUE REALIZADO COM SUCESSO')
+                        print(f'SALDO {saldo:.2f}')
+                        input('APERTE QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL... ')
+                        os.system('cls')
+                        break
+                    elif saque > LIMITE_VALOR:
+                        print('VALOR EXCEDIDO...')
+                        input('APERTE QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL... ')
+                        os.system('cls')
+                        break
+                    elif saques  == LIMITE_SAQUE:
+                        print('LIMITE EXCEDIDO...')
+                        input('APERTE QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL... ')
+                        os.system('cls')
+                        break
+                    elif saldo < saque:
+                        print('SALDO EM CONTA EXCEDIDO...')
+                        print(f'SALDO EM CONTA {saldo:.2f}')
+                        input('APERTE QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL... ')
+                        os.system('cls')
+                        break    
+                    elif config_2 == 'N':
+                        tentativa = input("""DESEJA TENTAR NOVAMENTE ?
+                                        [S] SIM
+                                        [N] NÃO
+                                        DIGITE : """).upper()
+                        os.system('cls')
+                        if (tentativa != 'S') or (tentativa != 'N'):
+                            break
+            except:
+                print('DIGITE APENAS VALORES NUMÉRICOS....')
+                input()
+                os.system('cls')
+                break           
+    elif opcao == 'E':
+        if len(extrato) > 0:
+            for i in extrato:
+                print(i)
+            input('APERTE QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL... ')
+            os.system('cls') 
+        else:
+            print('NÃO TEMOS EXTRATO...')
+            input('APERTE QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL... ')
+            os.system('cls') 
+    elif opcao  == 'Q':
         break
-    else:
-        print("OPÇÃO INVALIDA, TENTE NOVAMENTE")
-        
